741a6f8b6841d152879ea0113c68464bf36da927
Ricardo Cortes <32460439+RalesCortes@users.noreply.github.com>
2024-06-06 02:30:20 -0600

    alchemy

--- src/handlers/login.py
+++ src/handlers/login.py
@@ -4,6 +4,6 @@ from sqlalchemy import create_engine
 
 
 def handler(event, context):
-    cnx = create_engine('mysql://admin:password@mymysqlinstance.cbyoyq0e6uzw.us-east-1.rds.amazonaws.com/VictoriaProFitnessDb')    
+    cnx = create_engine('mysql+pymysql://admin:password@mymysqlinstance.cbyoyq0e6uzw.us-east-1.rds.amazonaws.com/VictoriaProFitnessDb')    
   
     return {}
